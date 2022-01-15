d7ed8648f1087ccf9e50e06311951b530d7a3a7a
Gadzhi Kharkharov <1460122+kkga@users.noreply.github.com>
2022-01-16 01:19:13 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Arrow } from "./Arrow";
+import "./styles.css";
+
+export default function App() {
+  const [start, setStart] = useState({ x: 0, y: 0 });
+  const [end, setEnd] = useState({ x: 0, y: 0 });
+
+  function handleMouseMove(ev: React.MouseEvent) {
+    setEnd({ x: ev.clientX, y: ev.clientY });
+  }
+
+  function handleClick(ev: React.MouseEvent) {
+    setStart({ x: ev.clientX, y: ev.clientY });
+  }
+
+  const windowSize = { x: window.innerWidth, y: window.innerHeight };
+
+  return (
+    <div
+      onMouseMove={handleMouseMove}
+      onClick={handleClick}
+      style={{ width: windowSize.x, height: windowSize.y }}
+    >
+      <Arrow
+        showControlPoints={true}
+        size={windowSize}
+        offsetMult={0.35}
+        minOffset={100}
+        start={start}
+        end={end}
+      />
+    </div>
+  );
+}
